ad2ff2652fb6176d49c914346ffb8e5fe8bfcb5b
ckc <chitkhonncho9@gmail.com>
2023-07-22 13:09:24 +0630

    when click play button, get data

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   
 <h1>How fast can you catch me!!</h1>
-
+<button @click="start" :disabled="isPlaying">Play</button>
+<div v-if="isPlaying">
+  <Block :delay="delay" />
+</div>
 </template>
 
 <script>
+import Block from "./components/Block.vue"
 
 export default {
   name: 'App',
- 
+  data(){
+    return{
+      isPlaying:false,
+      delay:null
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying=true;
+      this.delay=2000+Math.random()*5000;
+    }
+  },
+  components:{
+    Block
+  }
 }
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="block">Click Here   {{delay}} </div>
+</template>
+
+<script>
+export default {
+    props:['delay']
+}
+</script>
+
+<style>
+    .block{
+        background-color: aqua;
+        width: 400px;
+        height:300px;
+        margin:20px auto;
+        padding-top:30px;
+    }
+</style>
