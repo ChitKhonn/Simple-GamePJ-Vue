b4c27f43962b3768a9c9033d51e429a083a746a6
ckc <chitkhonncho9@gmail.com>
2023-07-22 19:06:38 +0630

    add delay props and use mounted

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="block">Click Here   {{delay}} </div>
+  <div class="block" v-if="showBlock">Click Here {{ delay }}</div>
 </template>
 
 <script>
 export default {
-    props:['delay']
-}
+  props: ["delay"],
+
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+
+  mounted(){
+    setTimeout(() => {
+        this.showBlock=true;
+    }, this.delay);
+  }
+};
 </script>
 
 <style>
-    .block{
-        background-color: aqua;
-        width: 400px;
-        height:300px;
-        margin:20px auto;
-        padding-top:30px;
-    }
+.block {
+  background-color: aqua;
+  width: 400px;
+  height: 300px;
+  margin: 20px auto;
+  padding-top: 30px;
+}
 </style>
