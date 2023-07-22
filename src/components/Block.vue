4658d5866f838585b0fa9d081b113b27aba20d4e
ckc <chitkhonncho9@gmail.com>
2023-07-22 21:17:05 +0630

    add startTimer and stopTimer fuction

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">Click Here {{ delay }}</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Here {{ delay }}</div>
 </template>
 
 <script>
@@ -9,13 +9,27 @@ export default {
   data() {
     return {
       showBlock: false,
+      score:0,
+      timer:null,
     };
   },
 
   mounted(){
     setTimeout(() => {
         this.showBlock=true;
+        this.startTimer();
+        
     }, this.delay);
+  },
+  methods:{
+    startTimer(){
+        this.timer = setInterval(() => {
+            this.score += 50;
+        }, 50);
+    },
+    stopTimer(){
+        clearInterval(this.timer())
+    }
   }
 };
 </script>
